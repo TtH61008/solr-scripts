76b8c5cb9313d2f3fdc6253928fdf9d43ca868c1
TtH61008 <imaseki1008@gmail.com>
2020-02-29 16:20:38 +0900

    setup.shの追加

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+sudo apt-get install openjdk-11-jdk -y
+
+wget https://archive.apache.org/dist/lucene/solr/8.3.1/solr-8.3.1.tgz
+tar xzf solr-8.3.1.tgz solr-8.3.1/bin/install_solr_service.sh --strip-components=2
+sudo bash ./install_solr_service.sh solr-8.3.1.tgz

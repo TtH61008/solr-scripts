e76b62b3cc0fde39f48d12fcf0910d2380e4d295
TtH61008 <imaseki1008@gmail.com>
2020-06-22 22:45:29 +0900

    category column / cut schema

--- fukuoka_preprocess.sh
+++ fukuoka_preprocess.sh
@@ -4,12 +4,12 @@ wget https://ckan.open-governmentdata.org/dataset/6255d72b-991f-48a3-8439-20b11f
 wget https://ckan.open-governmentdata.org/dataset/6255d72b-991f-48a3-8439-20b11fc106ab/resource/fa5da814-6fb9-4800-845f-7387ca0b9fd4/download/401307_public_facility_museum.csv
 
 #echo "city_code,no,prefecture,city,name,name_kana,name_pop,poi,address,calligraphy,latitude,longitude,phone_number,extension_number,corporate_number,org_name,available_days,start_time,end_time,available_time_remarks,explanation,easy_access_info,url,remarks" > fukuoka_converted.csv 
-echo "name,name_kana,name_pop,address,latitude,longitude,geo_p,phone_number" > fukuoka_converted.csv 
+echo "name,name_kana,name_pop,address,latitude,longitude,geo_p,phone_number,category" > fukuoka_converted.csv 
 
-tail -n +2 401307_public_facility_sports.csv |  awk 'BEGIN {FS=",";OFS=","} {print $5,$6,$7,$9,$11,$12,"\""$11,$12"\"",$13} ' >> fukuoka_converted.csv
-tail -n +2 401307_public_facility_library.csv  | awk 'BEGIN {FS=",";OFS=","} {print $5,$6,$7,$9,$11,$12,"\""$11,$12"\"",$13} ' >> fukuoka_converted.csv 
-tail -n +2 401307_public_facility_culture.csv  | awk 'BEGIN {FS=",";OFS=","} {print $5,$6,$7,$9,$11,$12,"\""$11,$12"\"",$13} ' >> fukuoka_converted.csv 
-tail -n +2 401307_public_facility_museum.csv | awk 'BEGIN {FS=",";OFS=","} {print $5,$6,$7,$9,$11,$12,"\""$11,$12"\"",$13} ' >> fukuoka_converted.csv 
+tail -n +2 401307_public_facility_sports.csv |  awk 'BEGIN {FS=",";OFS=","} {print $5,$6,$7,$9,$11,$12,"\""$11,$12"\"",$13,"sports"} ' >> fukuoka_converted.csv
+tail -n +2 401307_public_facility_library.csv  | awk 'BEGIN {FS=",";OFS=","} {print $5,$6,$7,$9,$11,$12,"\""$11,$12"\"",$13,"library"} ' >> fukuoka_converted.csv 
+tail -n +2 401307_public_facility_culture.csv  | awk 'BEGIN {FS=",";OFS=","} {print $5,$6,$7,$9,$11,$12,"\""$11,$12"\"",$13,"culture"} ' >> fukuoka_converted.csv 
+tail -n +2 401307_public_facility_museum.csv | awk 'BEGIN {FS=",";OFS=","} {print $5,$6,$7,$9,$11,$12,"\""$11,$12"\"",$13,"museum"} ' >> fukuoka_converted.csv 
 
 
-curl 'http://localhost:8983/solr/tokyo_conv/update?commit=true&indent=true' --data-binary @tokyo_conv.csv -H 'Content-Type: text/csv'
+# curl 'http://localhost:8983/solr/tokyo_conv/update?commit=true&indent=true' --data-binary @tokyo_conv.csv -H 'Content-Type: text/csv'
